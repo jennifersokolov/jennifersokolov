ded2b6042c6d1e88a02305f7fc62beb85c245726
andypbrowne <andypbrowne@gmail.com>
2019-08-19 22:50:15 -0400

    add five widgets

--- admin/preview-templates/page.js
+++ admin/preview-templates/page.js
@@ -9,10 +9,13 @@ const Page = createClass({
 
     return html`
       <main>
-        <h1>${entry.getIn(["data", "title"], null)}</h1>
-
-        ${this.props.widgetFor("body")}
-        
+        <div class="fill-box fill-box-gg">
+          <h1 class="hero-text">${this.props.widgetFor("intro")}</h1>
+        </div>
+          ${this.props.widgetFor("image")}
+          ${this.props.widgetFor("process")}
+          ${this.props.widgetFor("offerings")}
+          ${this.props.widgetFor("fees")}
         
       </main>
     `;
